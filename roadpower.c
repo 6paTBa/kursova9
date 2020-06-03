d8ecb8a3ece0d96c2ee4165d4baf30654afc91ff
Maxim <charleselvisbrooks@gmail.com>
2020-06-03 18:58:55 +0700

    Comments in shortest_way and extract_min funcs

--- roadpower.c
+++ roadpower.c
@@ -20,6 +20,8 @@ int extract_min(graph *graph_prot, int source, int *seen) {
             (graph_prot->roads[source][i] != 0) && (graph_prot->roads[source][i] < min)) {
                 return i;
         }
+        //если вершина раннее не просматривалась, если она смежна с source
+        //и расстояние от source до нее минимальный, то возвращаем эту вершину
     }
 }
 
@@ -27,38 +29,60 @@ uint16_t shortest_way(graph *graph_prot, int start, int finish)
 {
     int max = graph_prot->nvertices, v;
     int *d = malloc(sizeof(int) * graph_prot->nvertices);
+    //указатель на массив чисел, в котором хранятся текущие расстояния
+    //от стартовой вершины до [n-ой] вершины
     int *seen = calloc(graph_prot->nvertices, sizeof(int));
+    //указатель на массив просмотренных вершин
+    //если вершина n просмотрена, но в [n-ый] равен 1
     int *prev = malloc(sizeof(int) * graph_prot->nvertices);
+    //указатель на массив предыдущих в пути вершин
+    //предыдущая за n-ой вершиной - есть элемент [n] массива
 
     for(int i = 0; i < max; i++) {
         if(i != start) {
             d[i] = 32767;
         }
+        //для каждой вершины, кроме старта, показатель расстояния 
+        //от старта будет равняться максимальному значению int
         prev[i] = -1;
     }
     d[start] = 0;
 
     for(int i = 0; i < max; i++) {
         v = extract_min(graph_prot, start, seen);
+        //из множества непросмотренных вершин
+        //извлекаем самую ближнюю
         seen[v] = 1;
+        //отмечаем ее как просмотренную
         d[v] = graph_prot->roads[start][v];
 
         for(int j = 0; j < max; j++) {
+        //просматриваем все смежные с v вершины
             if(graph_prot->roads[v][j] != 0) {
+            //если между v и j есть дорога
                 if(graph_prot->roads[start][j] != 0) {
+                //если между стартом и j есть дорога
                     d[j] = graph_prot->roads[start][j];
                     if(d[v] + graph_prot[v][j] < d[j]) {
                         d[j] = d[v] + graph_prot[v][j];
                         prev[j] = v;
                     }
+                    //сравниваем, будет ли путь через v короче
+                    //чем напрямую от старта
+                    //если да, то значение расстояния от старта до j
+                    //будет равно расстоянию до j через v
                 } else {
                     d[j] = d[v] + graph_prot[v][j];
                     prev[j] = v;
                 }
+                //если от старта нет дороги до j
+                //значение расстояния от старта до j
+                //будет равно расстоянию до j через v
             }
         }
     }
     return d[finish];
+    //возвращаем минимальный путь до конечной вершины
 }
 
 uint16_t longest_way(graph *graph_prot, int start, int finish)
