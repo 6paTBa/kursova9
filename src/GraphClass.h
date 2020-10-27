a73e59a713af8f485fc4f4843fe337b0ead4a9e6
Димон <xedfr47@mail.ru>
2020-10-27 11:07:10 +0700

    Include path was added

---
+++ src/GraphClass.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_CLASS
+#define GRAPG_CLASS
+
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <list>
+#include <cassert>
+
+using namespace std;
+typedef unsigned int uint;
+
+template <typename ident_type, typename price_type> class Graph
+{
+
+};
+
+#endif
