bd962c69da701f5eba933337838d7ee0184e204d
Donchu942 <donchu0601@gmail.com>
2020-11-08 21:31:53 +0700

    Adding add command

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,103 @@ int main()
 
 	while(true)
 	{
-		if(cur_comand == "add") {}
+		if(cur_comand == "add") 
+		{
+			if(total_args < 3)
+			{
+				cerr << "Too few arguments for \"add\"" << endl;
+				continue;
+			}
+			string mode; s_strm >> mode; 
+			if(mode.size() != 1)
+			{
+				cerr << "unrecognized mode: \"" << mode << "\"" << endl;
+				continue;
+			}
+			string name_1, name_2;
+			s_strm >> name_1; s_strm >> name_2; 
+			uint price; s_strm >> price;
+			switch(mode[0]) 
+			{
+				case 'u':
+				{
+					auto add_result = graph.add_unoriented(name_1, name_2, price);
+					if(!add_result.second.second)
+					{
+						cout << "Route from \"" << add_result.first->first << "\" to \"" <<
+							add_result.second.first->first << "\" is alredy added for "
+							<< add_result.second.first->second << endl;
+						cout << "Change price? y/n" << endl;
+						string change;
+						while(1)
+						{
+							cin >> change;
+							cin.ignore();
+							if(change.size() != 1)
+							{
+								cerr << "unrecognized answer: \"" << change << "\"" << endl;
+								continue;
+							}
+							switch(change[0])
+							{
+								case 'y':
+									add_result.second.first->second = price;
+									break;
+								case 'n':
+									break;
+								default:
+									cerr << "unrecognized answer: \"" << change << "\"" << endl;
+									continue;
+									break;
+							}
+							break;
+						}
+					}
+					break;
+				}
+				case 'o':
+				{
+					auto add_result = graph.add_oriented(name_1, name_2, price);
+					if(!add_result.second.second)
+					{
+						cout << "Route from \"" << add_result.first->first << "\" to \"" <<
+							add_result.second.first->first << "\" is alredy added for "
+							<< add_result.second.first->second << endl;
+						cout << "Change price? y/n" << endl;
+						string change;
+						while(1)
+						{
+							cin >> change;
+							cin.ignore();
+							if(change.size() != 1)
+							{
+								cerr << "unrecognized answer: \"" << change << "\"" << endl;
+								continue;
+							}
+							switch(change[0])
+							{
+								case 'y':
+									add_result.second.first->second = price;
+									break;
+								case 'n':
+									break;
+								default:
+									cerr << "unrecognized answer: \"" << change << "\"" << endl;
+									continue;
+									break;
+							}
+							break;
+						}
+					}
+					break;
+				}
+				default:
+					cerr << "unrecognized mode: \"" << mode << "\"" << endl;
+					continue;
+			}
+			
+			continue;
+		}
 		else if(cur_comand == "longest") {}
 		else if(cur_comand == "shortest") {}
 		else if(cur_comand == "all") {}
