8314fd4172b873eadc3e694442d2e1992e72d554
Maxim <charleselvisbrooks@gmail.com>
2020-11-10 16:23:07 +0700

    Wrote shortest route function

--- src/Func.cpp
+++ src/Func.cpp
@@ -2,12 +2,22 @@
 
 template <typename ident_type, typename price_type> list<ident_type> find_shortest_route(const Graph<ident_type, price_type>& graph, ident_type from, ident_type to)
 {
+	for(auto& edges : graph)
+	{
+		for(auto& prices : edges.second)
+			assert(prices.second >= 0 && "Shoud be greater than zero");
+	}
+
     list<ident_type> route;
 	if(from == to)
 	{
 		route.push_front(to);
 		return route;
 	}
+
+	unordered_map<ident_type, ident_type> lead_from;
+	for(auto it = cur_prices.begin(); it != cur_prices.end(); it++)
+		lead_from[it->first] = from;
 
     typename Graph<ident_type, price_type>::price_hash cur_prices = graph[from];
 	typename Graph<ident_type, price_type>::price_iterator cur_min = cur_prices.begin();
@@ -16,8 +26,53 @@ template <typename ident_type, typename price_type> list<ident_type> find_shorte
 	visited.insert(from);
 
     while(!cur_prices.empty())
-    {  
+    {
+		cur_min = cur_prices.begin();
+		for(auto it = cur_prices.begin(); it != cur_prices.end(); it++)
+			if(it->second < cur_min->second) cur_min = it;
+		
+		if(cur_min->first == to) 
+		{
+			while(lead_from.find(to) != lead_from.end())
+			{
+				route.push_front(to);
+				to = lead_from[to];
+			}
+			break;
+		}
+		
+		auto& cur_min_possible_ways = graph[cur_min->first];
+		for(auto it = cur_min_possible_ways.begin(); it != cur_min_possible_ways.end(); it++)
+		{
+			if(visited.find(it->first) == visited.end())
+			{
+				auto already_in_prices = cur_prices.find(it->first);
+				if(already_in_prices != cur_prices.end())
+				{
+					if(already_in_prices->second > it->second + cur_min->second)
+					{
+						already_in_prices->second = it->second + cur_min->second;
+						lead_from[already_in_prices->first] = cur_min->first;
+					}
+				}
+				else 
+				{
+					auto inserted = cur_prices.insert(*it);
+					inserted.first->second += cur_min->second;
+					lead_from[it->first] = cur_min->first;
+				}
+			}
+		}
+		visited.insert(cur_min->first);
+		cur_prices.erase(cur_min);
     }
+
+	if(route.empty()) return route;
+	else
+	{
+		route.push_front(from);
+		return route;
+	}
 }
 
 template <typename ident_type, typename price_type> 
