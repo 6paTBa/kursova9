51b95001a3e8bc56807e910f57f401e02c6a3100
Димон <xedfr47@mail.ru>
2020-11-19 14:58:03 +0700

    First tests added

--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,36 @@
 #include "GraphClass.h"
 #include <ctest.h>
+
+CTEST(is_exist_ident_check, unknown_ident)
+{
+    Graph<string, uint> graph;
+    string ident = "city_name";
+    ASSERT_FALSE(graph.is_exist(ident));
+}
+
+CTEST(is_exist_ident_check, added_ident)
+{
+    Graph<string, uint> graph;
+    string ident_1 = "city_1", ident_2 = "city_2";
+    uint route = 1;
+    graph.add_oriented(ident_1, ident_2, route);
+    ASSERT_TRUE(graph.is_exist(ident_1));
+}
+
+CTEST(is_exist_route_check, unknown_route)
+{
+    Graph<string, uint> graph;
+    string ident_1 = "city_1", ident_2 = "city_2";
+    uint route = 1;
+    graph.add_oriented(ident_1, ident_2, route);
+    ASSERT_FALSE(graph.is_exist(ident_2, ident_1));
+}
+
+CTEST(is_exist_route_check, added_route)
+{
+    Graph<string, uint> graph;
+    string ident_1 = "city_1", ident_2 = "city_2";
+    uint route = 1;
+    graph.add_oriented(ident_1, ident_2, route);
+    ASSERT_TRUE(graph.is_exist(ident_1, ident_2));
+}
